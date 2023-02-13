4e3a151ed8374523621ea102ca4ecdf2dcb9ec1b
sukkwono <sukkwono@sfu.ca>
2023-02-13 02:08:24 -0800

    stock: add views

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import (
+    Company,
+    CompanySerializer,
+    PriceHistory,
+    PriceHistorySerializer,
+    Stock,
+    StockSerializer,
+)
+
+
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    # permission_classes = [IsAdminUser]
+
+
+class StockList(generics.ListCreateAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # permission_classes = [IsAdminUser]
+
+
+class PriceHistoryList(generics.ListCreateAPIView):
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+    # permission_classes = [IsAdminUser]
