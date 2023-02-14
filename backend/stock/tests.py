9c3c73d8fec6e05bfe79c3c43010acdfb1be615f
sukkwono <sukkwono@sfu.ca>
2023-02-14 15:51:21 -0800

    add test cases

--- backend/stock/tests.py
+++ backend/stock/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class StockRetrieveUpdateDestroyTestCase(TestCase):
+    def test_retrieve(self):
+        """_summary_
+        Test GET (retrieve) request.
+        """
+        response = self.client.get(reverse("stock-rud", args=["NFLX"]))
+        self.assertEqual(response.status_code, 200)
