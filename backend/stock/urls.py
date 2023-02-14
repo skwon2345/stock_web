d39164ed262483634745ea3ae12701f1577f5924
sukkwono <sukkwono@sfu.ca>
2023-02-14 15:51:41 -0800

    be: stock: add name to url

--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -4,7 +4,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("stock/", views.StockListCreate.as_view()),
-    path("stock/<str:symbol>", views.StockRetrieveUpdateDestroy.as_view()),
-    path("candle/", views.CandleListCreate.as_view()),
+    path("stock/", views.StockListCreate.as_view(), name="stock-lc"),
+    path(
+        "stock/<str:symbol>",
+        views.StockRetrieveUpdateDestroy.as_view(),
+        name="stock-rud",
+    ),
+    path("candle/", views.CandleListCreate.as_view(), name="candle-lc"),
 ]
