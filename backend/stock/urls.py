e1d8722a9910505dea454ecca32d4090b9e77a33
sukkwono <sukkwono@sfu.ca>
2023-02-14 19:47:24 -0800

    change url name

--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -4,11 +4,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("stock/", views.StockListCreate.as_view(), name="stock-lc"),
+    path("stock/", views.StockList.as_view(), name="stock-l"),
     path(
         "stock/<str:symbol>",
         views.StockRetrieveUpdateDestroy.as_view(),
         name="stock-rud",
     ),
-    path("candle/", views.CandleListCreate.as_view(), name="candle-lc"),
+    path("candle/", views.CandleList.as_view(), name="candle-l"),
 ]
