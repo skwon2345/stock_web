910311c42a623ccea004c03f5041685a2d290f30
sukkwono <sukkwono@sfu.ca>
2023-02-15 20:01:19 -0800

    fe: header: add nav bar examples

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './styles.css'
 
 const Header = () => {
@@ -37,9 +38,49 @@ function NavItem(props) {
 }
 
 function DropdownMenu() {
-    return (
-        <div className='dropdown'>
 
+    const [activeMenu, setActiveMenu] = useState('main'); // settings, anmials
+    const [menuHeight, setMenuHeight] = useState(null);
+
+    function calcHeight(el) {
+        const height = el.offsetHeight;
+        setMenuHeight(height);
+    }
+
+
+    function DropdownItem(props) {
+        return (
+            <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+                <span className='icon-button'>{props.leftIcon}</span>
+                {props.children}
+                <span className='icon-right'>{props.rightIcon}</span>
+            </a>
+        )
+    }
+    return (
+        <div className='dropdown' style={{ height: menuHeight }}>
+            <CSSTransition in={activeMenu === "main"} unmountOnExit timeout={500} classNames="menu-primary" onEnter={calcHeight}>
+                <div className='menu'>
+                    <DropdownItem>My Profile</DropdownItem>
+                    <DropdownItem leftIcon="left" rightIcon="right" goToMenu="settings">Settings</DropdownItem>
+                </div>
+            </CSSTransition>
+            <CSSTransition in={activeMenu === "settings"} unmountOnExit timeout={500} classNames="menu-secondary" onEnter={calcHeight}>
+                <div className='menu'>
+                    <DropdownItem leftIcon="<-" goToMenu="main"></DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                </div>
+            </CSSTransition>
         </div>
     );
 }
