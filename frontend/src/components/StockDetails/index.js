83086260da0b8f3d86013e69b65306c30fbfa554
Nayoung <skdud3669@gmail.com>
2023-02-16 17:34:00 -0800

    fe:stockdetails:fetch chart data

--- frontend/src/components/StockDetails/index.js
+++ frontend/src/components/StockDetails/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { useParams} from 'react-router-dom';
 import { DataContext } from '../../context/DataProvider'
+import axios from 'axios';
+
 import './styles.css';
 
 const StockDetails = () => {
-    const symbol  = useParams().symbol;
-    console.log(symbol)
-  
-    return (
-      <div className='stockDetails'>
-        <div>{symbol}</div>
-      </div>
+  const [chartData, setChartData] = useState([]);
+  const {stockData} = useContext(DataContext);
+  const symbol  = useParams().symbol;
+  const [stock] =  stockData.filter(stock => { return stock.symbol === symbol})
+
+const callback = useCallback(async () => {
+  const url = `/api/candle/?symbol=${stock.symbol}&from=1900-01-01&to=2023-02-16`;
+  axios.get(url)
+  .then(function(response) {
+      console.log(response.data);
+      setChartData(response.data);
+  })
+  .catch(function(error) {
+      console.log("실패(차트)");
+  })
+}, [stock])
+
+
+  useEffect(() => {
+    callback();
+  },[])  
+
+  return (
+    <div className='stockDetails'>
+      <div>{stock.name}</div>
+    </div>
   )
 }
 
