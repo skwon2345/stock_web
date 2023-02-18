b033dae25fcc34849473d58dae5be56534ec198e
Nayoung <skdud3669@gmail.com>
2023-02-18 00:30:36 -0800

    fe:stockdetails: added chart (apexcharts)

--- frontend/src/components/StockDetails/index.js
+++ frontend/src/components/StockDetails/index.js
@@ -8,28 +8,13 @@ import './styles.css';
 const StockDetails = () => {
   const [chartData, setChartData] = useState([]);
   const [stockData, setStockData] = useState({});
-  const [isLoading, setLoading] = useState();
   const location = useLocation();
-  const [options, setObject] = useState({
-    chart: {
-      type: 'candlestick',
-      height: 500
-    },
-    title: {
-      text: 'Chart',
-      align: 'left',
-    },
-
-  })
-
-
   const stock_symbol = location.state;
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       const stockData_url = `/api/stock/${stock_symbol}`
-      const chart_url = `/api/candle/?symbol=${stock_symbol}&from=1900-01-01&to=2023-02-16`;
+      const chart_url = `/api/candle/?symbol=${stock_symbol}&from=2020-01-01&to=2023-02-16`;
 
       await axios.get(stockData_url)
       .then(function(response) {
@@ -48,15 +33,8 @@ const StockDetails = () => {
       .catch(function(error) {
           console.log("실패(차트)");
       })
-      setLoading(false);
     }  
-    fetchData();
-
-    const value=[]
-    for (let i=0; i< chartData.length; i++) {
-      value.push({x: chartData[i].date, y: [chartData[i].open, chartData[i].high, chartData[i].low, chartData[i].close]})
-    }
-
+    fetchData();  
   },[]);
 
   if (chartData.length === 0) {
@@ -65,42 +43,90 @@ const StockDetails = () => {
     return null;
   }
 
-  console.log("--------")
-  console.log(stockData)
-  console.log(chartData)
+  // console.log("--------")
+  // console.log(stockData)
+  // console.log(chartData)
 
-  
+
     return (
     <div className='stockDetails'>
-      {/* <ReactApexChart options={options} series={series} type="candlestick" height={350} /> */}
+      <Chart 
+        type="candlestick" 
+        series={[
+          {
+            data: chartData?.map((item) => {
+              return [
+                Date.parse(item.date),
+                item.open,
+                item.high,
+                item.low,
+                item.close,
+              ]
+            })
+          }
+        ]}
+        options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              type: "candlestick",
+              height: 350,
+              width: 500,
+              background: "rgb(67, 67, 67)",
+              zoom: {
+                enabled: true,
+                type: 'x',  
+                autoScaleYaxis: true,  
+                zoomedArea: {
+                  fill: {
+                    color: '#90CAF9',
+                    opacity: 0.4
+                  },
+                  stroke: {
+                    color: '#0D47A1',
+                    opacity: 0.4,
+                    width: 1
+                  }
+                }
+              },
+            },
+            xaxis: {
+              type: "datetime",
+              categories: chartData.map((item) => item.date),
+              labels: {
+                style: {
+                  fontSize: '13px',
+                  colors: '#ffffff',
+                },
+                orientation: 'horizontal',
+              },
+            },
+            yaxis: {
+              tooltip: {
+                enabled: true
+              },
+              forceNiceScale: true,
+              labels: {
+                style: {
+                  fontSize: '13px',
+                  colors: '#ffffff',
+                },
+            },
+            plotOptions: {
+              candlestick: {
+                  colors: {
+                      upward: '#3C90EB',
+                      downward: '#DF7D46'
+                  },
+              },
+            },
+            
+          }}
+        }
+      />
     </div>
     )
   }
 
 export default StockDetails;
-
-
-// close
-// : 
-// 62
-// date
-// : 
-// "2019-04-18"
-// high
-// : 
-// 66
-// id
-// : 
-// 3182
-// low
-// : 
-// 60.321
-// open
-// : 
-// 65
-// symbol
-// : 
-// "ZM"
-// volume
-// : 
-// 25764659
