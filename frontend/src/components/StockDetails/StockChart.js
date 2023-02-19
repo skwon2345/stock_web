75ac76cab457644fd1c3f73cbeb6da107735ae5c
Nayoung <skdud3669@gmail.com>
2023-02-19 00:00:54 -0800

    fe:stockdetail: chart async loading

--- frontend/src/components/StockDetails/StockChart.js
+++ frontend/src/components/StockDetails/StockChart.js
@@ -1,207 +1,144 @@
-import React from 'react'
-import Chart from 'react-apexcharts'
+import React, { useState, useEffect } from "react";
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import Indicators from "highcharts/indicators/indicators-all.js";
+import DragPanes from "highcharts/modules/drag-panes.js";
+import AnnotationsAdvanced from "highcharts/modules/annotations-advanced.js";
+import PriceIndicator from "highcharts/modules/price-indicator.js";
+import FullScreen from "highcharts/modules/full-screen.js";
+import StockTools from "highcharts/modules/stock-tools.js";
+import axios from 'axios';
 import './styles.css'
 
-export const StockChart = ({chartData}) => {
+
+export const StockChart =({stock_symbol}) => {
+    const [chartData, setChartData] = useState([]);
+
+    const ohlc=[]
+    const volume=[]
+    console.log("sdf")
+
+    Indicators(Highcharts);
+    DragPanes(Highcharts);
+    AnnotationsAdvanced(Highcharts);
+    PriceIndicator(Highcharts);
+    FullScreen(Highcharts);
+    StockTools(Highcharts);
+
+    useEffect(()=> {
+        
+    })
+
+    function afterSetExtremes(e) {
+        const { chart } = e.target;
+        chart.showLoading('Loading data from server...');
+        fetch(`/api/candle/?symbol=${stock_symbol}&from=2022-01-01&to=2023-02-16$?start=${Math.round(e.min)}&end=${Math.round(e.max)}`)
+            .then(res => res.ok && res.json())
+            .then(data => {
+                chart.series[0].setChartData(data);
+                console.log("성공(차트)")
+                chart.hideLoading();
+            }).catch(error =>  console.log("실패(차트)"));
+    }
+
+    if (chartData.length === 0) {
+        return null;
+    } 
+
+    chartData?.map((item) => {
+        ohlc.push([Date.parse(item.date), item.open, item.high, item.low, item.close])
+        volume.push([Date.parse(item.date), item.volume])
+    })
+    // Add a null value for the end date
+    chartData.push([Date.UTC(2011, 9, 14, 18), null, null, null, null]);
+
+    const options = {
+        plotOptions: {
+            ohlc: {
+                color: 'red',
+                upColor: 'green'
+            },
+        },
+        series : [{
+            type: 'ohlc',
+            name : 'Stock Price', 
+            data : ohlc,
+            yAxis : 0,
+        }, {   
+            type: 'column',
+            name : 'volume', 
+            data : volume,
+            yAxis : 1,
+        }],
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'AAPL Historical',
+            style : {
+                color: "#ffffff",
+            }
+            
+        },
+            chart: {
+            height: (9 / 16 * 100) + '%',
+            width: 1100,
+            backgroundColor: '#242526',
+            style : {
+                color: "#ffffff",
+            }
+            
+        },
+        yAxis: [{
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: 'OHLC'
+            },
+            
+            height: '60%',
+            lineWidth: 2,
+            resize: {
+                enabled: true
+            }
+        }, {
+            labels: {
+                align: 'right',
+                x: -3,
+                style : {
+                    color: "#ffffff",
+                }
+            },
+            title: {
+                text: 'Volume'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+        }],
+        xAxis: {
+                events: {
+                    afterSetExtremes: (e) => afterSetExtremes(e)
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+        tooltip: {
+            split: true
+        },
+    };
+
 
     return (
-        <div className='stockChart'>
-            <Chart
-            type="candlestick"
-            series={[
-            {
-                data: chartData?.map((item) => {
-                return [
-                    Date.parse(item.date),
-                    item.open,
-                    item.high,
-                    item.low,
-                    item.close,
-                ]
-                })
-            }
-            ]}
-            options={{
-                theme: {
-                    mode: 'dark',
-                },
-                chart: {
-                    type: "candlestick",
-                    height: 350,
-                    id: "candles",
-                    group: "candles",
-                    background: 'grey',
-                    toolbar: {
-                        autoSelected: "pan",
-                        show: false,
-                        },
-                    events: {
-                        scrolled: function(chart, { xaxis }) {
-                    // ...
-                    }
-                },
-                },
-                zoom: {
-                        enabled: true,
-                        type: 'x',
-                        autoScaleYaxis: true,
-                        resetIcon: {
-                            offsetX: -10,
-                            offsetY: 0,
-                            fillColor: '#fff',
-                            strokeColor: '#37474F'
-                        },
-                        zoomedArea: {
-                            fill: {
-                                color: '#90CAF9',
-                                opacity: 0.4
-                            },
-                        }
-                    },
-                
-                grid: {
-                    borderColor: "#555",
-                    clipMarkers: false,
-                    yaxis: {
-                    lines: {
-                        show: false
-                    }
-                    }
-                },
-                fill: {
-                    gradient: {
-                    enabled: true,
-                    opacityFrom: 0.55,
-                    opacityTo: 0
-                    }
-                },
-                xaxis: {
-                    type: "datetime",
-                    categories: chartData.map((item) => item.date),
-                    labels: {
-                        style: {
-                            fontSize: '13px',
-                            colors: '#ffffff',
-                        },
-                            orientation: 'horizontal',
-                        },
-                    tickPlacement: 'on'
-                },
-                yaxis: {
-                    tooltip: {
-                    enabled: true
-                    },
-                    forceNiceScale: true,
-                    labels: {
-                        style: {
-                            fontSize: '13px',
-                            colors: '#ffffff',
-                        },
-                    },
-                },
-                plotOptions: {
-                    candlestick: {
-                        colors: {
-                            upward: 'rgb(57, 77, 204)',
-                            downward: 'rgb(186, 71, 65)',
-                        },
-                    },
-                },
-            }}
-        />
-    </div>
-    )
-}
-
-
-export const StockChartBar = ({chartData}) => {
-    return (
-        <div className="subChart">
-            <Chart
-            type= 'bar'
-            series={[
-                {
-                    name: 'volume',
-                    data: chartData?.map((item) => {
-                            return [
-                                Date.parse(item.date),
-                                item.volume,
-                            ]
-                        })
-                }
-            ]}
-            options={{
-                chart: {
-                    type: "bar",
-                    height: 150,
-                    width: 500,
-                    group: "candles",
-                    id: "subchart",
-                    
-                    brush: {
-                        enabled: true,
-                        target: 'candles',
-                        autoScaleYaxis: true,
-                        
-                    },
-                    selection: {
-                        enabled: true,
-                        fill: {
-                            color: "#fff",
-                            opacity: 0.4
-                        },
-                        stroke: {
-                            color: '#64a59d',
-                        }
-                    },
-                },
-                grid: {
-                    borderColor: "#555",
-                    clipMarkers: false,
-                    yaxis: {
-                        lines: {
-                            show: false
-                        }
-                    }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                plotOptions: {
-                    candlestick: {
-                        colors: {
-                            upward: '#64a59d',
-                        },
-                        wick: {
-                            useFillColor: true,
-                        }
-                    }
-                },
-                fill: {
-                    gradient: {
-                        enabled: true,
-                        opacityFrom: 0.55,
-                        opacityTo: 0
-                    }
-                },
-                stroke: {
-                        width: 2
-                },
-                xaxis: {
-                    type: 'datetime',
-                    axisBorder: {
-                        offsetX: 13
-                        }
-                },
-                yaxis: {
-                    labels: {
-                        show: false
-                    },
-                    tickAmoutn: 2
-                }
-                }
-            }
-        />
+        <div className="stockChart">
+            <HighchartsReact
+                highcharts={Highcharts}
+                constructorType={"stockChart"}
+                options={options}
+            />
         </div>
     )
 }
--- frontend/src/components/StockDetails/index.js
+++ frontend/src/components/StockDetails/index.js
@@ -1,59 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom'
-import {StockChart, StockChartBar} from './StockChart'
+import { StockChart } from './StockChart'
 import './styles.css';
 
 const StockDetails = () => {
-  const [chartData, setChartData] = useState([]);
-  const [stockData, setStockData] = useState({});
+    const [stockData, setStockData] = useState([]);
   const location = useLocation();
   const stock_symbol = location.state;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const stockData_url = `/api/stock/${stock_symbol}`
-      const chart_url = `/api/candle/?symbol=${stock_symbol}&from=2022-01-01&to=2023-02-16`;
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const stockData_url = `/api/stock/${stock_symbol}`
 
-      await axios.get(stockData_url)
-      .then(function(response) {
-          // console.log(response.data);
-          setStockData(response.data);
-      })
-      .catch(function(error) {
-          console.log("실패(데이터)");
-      })
+  //     await axios.get(stockData_url)
+  //     .then(function(response) {
+  //         // console.log(response.data);
+  //         setStockData(response.data);
+  //     })
+  //     .catch(function(error) {
+  //         console.log("실패(데이터)");
+  //     })
 
-      await axios.get(chart_url)
-      .then(function(response) {
-          // console.log(response.data);
-          setChartData(response.data);
-      })
-      .catch(function(error) {
-          console.log("실패(차트)");
-      })
-    }  
-    fetchData();  
-  },[]);
+      
+  //   }  
+    // fetchData();  
+  // },[]);
 
-  if (chartData.length === 0) {
+  if (stockData.length === 0) {
     return null;
-  } else if (stockData.length === 0) {
-    return null;
-  }
+  } 
 
   return (
   <div className='stockDetails'>
-    <DetailHeader chartData={chartData}/>
+    <DetailHeader />
     <div className="board">
       <MenuBar />
       <div className='charts'>
-        <StockChart chartData={chartData}/>
-        <StockChartBar chartData={chartData} />
+        <StockChart stock_symbol={stock_symbol} />
+      </div>
     </div>
-  </div>
-
-    
   </div>
   )
   }
