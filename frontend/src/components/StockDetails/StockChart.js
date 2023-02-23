cedff9adcf8edb2c0dc6640aafa984bc452c411e
Nayoung <skdud3669@gmail.com>
2023-02-22 19:33:00 -0800

    fe:stockdetail: plotly chart

--- frontend/src/components/StockDetails/StockChart.js
+++ frontend/src/components/StockDetails/StockChart.js
@@ -1,233 +1,125 @@
-import React, { useState} from "react";
-import Highcharts from "highcharts/highstock";
-import HighchartsReact from "highcharts-react-official";
-import Indicators from "highcharts/indicators/indicators-all.js";
-import DragPanes from "highcharts/modules/drag-panes.js";
-import AnnotationsAdvanced from "highcharts/modules/annotations-advanced.js";
-import PriceIndicator from "highcharts/modules/price-indicator.js";
-import FullScreen from "highcharts/modules/full-screen.js";
-import StockTools from "highcharts/modules/stock-tools.js";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import Plot from 'react-plotly.js';
 import './styles.css'
 
+
 export const StockChart =({stock_symbol}) => {
+    const [data, setData] = useState([]);
     const dataURL = `/api/candle/?symbol=${stock_symbol}&from=1900-01-01&to=2023-02-20`
-    const [options, setOptions] = useState({
-        ...chartOptions,
-        chart: {
-            type: "candlestick",
-            zoomType: "x",
-        },
-        xAxis: {
-            events: {
-                afterSetExtremes
-            },
-            minRange: 3600 * 1000 // one hour
-        },
-        title: {
-            text: stock_symbol,
-            style : {
-                color: "#ffffff",
-            }
-        },
 
-    })
-    const loadInitialData = async () => {
-        if (options.hasOwnProperty("series")) {
-            return
-        } else {
-            var ohlc =[]
-            var volume = []
-            try {
-                const response = await axios.get(dataURL)
-                response.data.map(item => {
-                                ohlc.push([Date.parse(item.date), item.open, item.high, item.low, item.close])
-                                volume.push([Date.parse(item.date), item.volume])
-                            })
-                console.log(ohlc)
-            } catch (err) {
-                console.log("실패(차트)");
-            }
-            console.log(ohlc)
-            setOptions({
-                ...chartOptions,
-                series: [
-                    {
-                        data: ohlc,
-                        dataGrouping: {
-                            enabled: false
-                        },
-                        name: 'candlestick',
-                        type: "candlestick"
-                    },
-                    {   
-                        type: 'column',
-                        name : 'volume', 
-                        data : volume,
-                        yAxis : 1,
-                }
-                ]
+    useEffect(() => {
+        const initialValue = async () => {
+            await axios.get(dataURL)
+                .then(function(response) {
+                    var trace = {
+                        x: [],
+                        close: [],
+                        high: [],
+                        low: [],
+                        open: [],
+                        line: {color: 'rgba(31,119,180,1)'}, 
+                        decreasing: {line: {color: '#7F7F7F'}}, 
+                        increasing: {line: {color: '#17BECF'}},
+                        type: 'candlestick',
+                        yaxis: 'y',
+                        xaxis: 'x',
+                    }
+                    var volume = {
+                        x: [],
+                        y: [],
+                        type: 'bar',
+                        line: {color: 'grey'},
+                        yaxis: 'y',
+                        xaxis: 'x',
+                    }
+                response.data.map((item) => {
+                    trace.x.push(JSON.stringify(Date.parse(item.date)))
+                    trace.close.push(JSON.stringify(item.close))
+                    trace.high.push(JSON.stringify(item.high))
+                    trace.low.push(JSON.stringify(item.low))
+                    trace.open.push(JSON.stringify(item.open))
+                    volume.x.push(JSON.stringify(Date.parse(item.date)))
+                    volume.y.push(item.volume)
+                })
+                setData([trace,volume])
+        // console.log(volume)
+
             })
         }
-    };
+        initialValue();
+    }, [])
+    
 
-    Indicators(Highcharts);
-    DragPanes(Highcharts);
-    AnnotationsAdvanced(Highcharts);
-    PriceIndicator(Highcharts);
-    FullScreen(Highcharts);
-    StockTools(Highcharts);
-    loadInitialData();
-
-    function afterSetExtremes(e) {
-        const { chart } = e.target;
-        var url = null;
-
-        if (e.hasOwnProperty("rangeSelectorButton")) {
-            const endDateObj = new Date();
-            const startDateObj = new Date();
-            startDateObj.setDate(startDateObj.getDate()- ((e.rangeSelectorButton.text === "1m") ? 31 : 365));
-
-            const endDate = `${endDateObj.getFullYear()}-${endDateObj.getMonth()+1}-${endDateObj.getDate()}`;
-            const startDate = `${startDateObj.getFullYear()}-${startDateObj.getMonth()+1}-${startDateObj.getDate()}`;
-
-            console.log(endDate)
-            console.log(startDate)
-            url = `${dataURL}?start=${Math.round(e.min)}&end=${Math.round(e.max)}`
-
-            axios.get(url)
-            .then(function(response) {
-                const ohlc = []
-                response.data.map((item) =>
-                    ohlc.push([Date.parse(item.date), item.open, item.high, item.low, item.close]))
-                chart.series[0].setData(ohlc);
-                console.log(options.series[0])
-                chart.hideLoading();
-            })
-            .catch(error =>  console.log(error));
-
-
-        } 
-        else {
-            if (options.hasOwnProperty("series")) {
-                chart.showLoading('Loading data from server...');    
-            }
-        }
+    const config= {
+        autoscale:true,
+        autosize: true,
+        responsive: true,
     }
 
     return (
         <div className="stockChart">
-            <HighchartsReact
-                highcharts={Highcharts}
-                constructorType={"stockChart"}
-                options={options}
-            />
+            <Plot data={data} layout={layout} config={config}/>
         </div>
     )
 }
 
-const chartOptions = {
-        plotOptions: {
-            candlestick: {
-                color: 'red',
-                upColor: 'green'
-            },
-            series: {
-                allowPointSelect: true,
-                marker: {
-                    enabled: true,
-                    states: {
-                        hover: {
-                        enabled: true
-                        },
-                        select: {
-                            enabled: true,
-                            radius: 5
-                        }
-                    }
-                },
-            }
-                
-        },
-        chart: {
-            type:"candlestick",
-            zoomType: "x",
-            height: (9 / 16 * 100) + '%',
-            width: 1200,
-            backgroundColor: '#242526',
-            style : {
-                color: "#ffffff",
-            },
-        },
-        navigator: {
-            adaptToUpdatedData: false,
-        },
-        scrollbar: {
-            liveRedraw: false
-        },
-        rangeSelector: {
-            buttons: [
-            {
-                type: "month",
-                count: 1,
-                text: "1m"
-            },
-            {
-                type: "month",
-                count: 3,
-                text: "3m"
-            },
-            {
-                type: "year",
-                count: 1,
-                text: "1y"
-            },
-            // {
-            //     type: "year",
-            //     count: 3,
-            //     text: "3y"
-            // },
-            {
-                type: "all",
-                text: "All"
-            }
-            ],
-            allButtonsEnabled: true,
-            selected: 3,
-            enabled: true,
-        },
-        yAxis: [
-                {
-                    labels: {
-                        align: "right",
-                        x: -3
-                    },
-                    title: {
-                        text: "OHLC"
-                    },
-                    height: "60%",
-                    lineWidth: 2,
-                    resize: {
-                        enabled: true
-                    },
-                    gridLineColor: '#ffffff',
-                    gridLineWidth: 1,
-                },
-                {
-                    labels: {
-                        align: "right",
-                        x: -3
-                    },
-                    title: {
-                        text: "Volume"
-                    },
-                    top: "65%",
-                    height: "35%",
-                    offset: 0,
-                    lineWidth: 2
-                }
-        ],
-        tooltip: {
-            split: true
-        }
-    }
+var selectorOptions = {
+    buttons: [{
+        step: 'month',
+        stepmode: 'backward',
+        count: 1,
+        label: '1m'
+    }, {
+        step: 'month',
+        stepmode: 'backward',
+        count: 6,
+        label: '6m'
+    }, {
+        step: 'year',
+        stepmode: 'todate',
+        count: 1,
+        label: 'YTD'
+    }, {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1y'
+    }, {
+        step: 'all',
+    }],
+    selected: 2
+};
+
+const layout = {
+    font: {size: 13},
+    width: 1100,
+    height: 780,
+    xaxis: {
+        autorange: true, 
+        showgrid: true,
+        // rangeslider: {range: ['2020-01-03 12:00', '2020-12-03 12:00']}, 
+        type: 'date',
+        rangeselector: selectorOptions,
+        rangeslider: {},
+        
+    }, 
+    yaxis: {
+            autorange: true, 
+            type: 'candlestick',
+            boundmode: 'hard',
+            bounds: [50, 100],
+            anchor: "x",
+            overlaying: "y"
+    },
+    yaxis2: {
+            autorange: false, 
+            type: 'candlestick',
+            boundmode: 'hard',
+            bounds: [50, 100],
+            overlaying: 'y',
+            side: "right"
+    },
+    plot_bgcolor:"#242526",
+    paper_bgcolor:"#242526"
+};
